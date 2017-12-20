d0e1d0ed0784cc0f283e73b57961382c1a5e6030
刘彪 <liubiao@liubiaodeMacBook-Pro.local>
2017-12-20 10:55:34 +0800

    clean node_modules log

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var  app = express()
 
 app.get('/', function(req, res) {
 	console.error("故意设置的error");
-    res.send('Hello Pm2');
+    res.send('Hello Pm2123123123');
 });
 
 app.listen(3000, function(){
