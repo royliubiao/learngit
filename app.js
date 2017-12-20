a679c89e96a87b9427756ea25171e9d08bb873be
刘彪 <liubiao@liubiaodeMacBook-Pro.local>
2017-12-20 11:30:04 +0800

    change port

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ var  app = express()
 
 app.get('/', function(req, res) {
 	console.error("故意设置的error");
-    res.send('Hello Pm2123123123');
+    res.send('Hello Pm 5000');
 });
 
-app.listen(3030, function(){
-	console.log('Example app listening on port 3000!')
+app.listen(5000, function(){
+	console.log('Example app listening on port 5000!')
 } )
